caad1b92cc0c70b82f224871a433b1ac5f2ca2e4
Patrick J. Franz <patrick@z-dyne.com>
2013-11-28 15:02:15 +0000

    Initial commit of cleanup script.

---
+++ scripts/cleanup.sh
@@ -0,0 +1,48 @@
+sudo rm -rf ~/python_games
+sudo apt-get -y autoremove python*
+sudo apt-get -y autoremove omxplayer
+sudo apt-get -y autoremove scratch
+sudo apt-get -y autoremove midori
+sudo apt-get -y autoremove dillo
+sudo apt-get -y autoremove lxde-common
+sudo apt-get -y autoremove hicolor-icon-theme
+sudo apt-get -y autoremove galculator
+sudo apt-get -y autoremove xarchiver
+sudo apt-get -y autoremove lxde-icon-theme
+sudo apt-get -y autoremove libpoppler19
+sudo apt-get -y autoremove x11-common
+sudo apt-get -y autoremove ed
+sudo apt-get -y autoremove vim-common
+sudo apt-get -y autoremove lxsession
+sudo apt-get -y autoremove lxappearance
+sudo apt-get -y autoremove lxpolkit
+sudo apt-get -y autoremove lxrandr
+sudo apt-get -y autoremove lxsession-edit
+sudo apt-get -y autoremove lxshorcut
+sudo apt-get -y autoremove lxtask
+sudo apt-get -y autoremove lxterminal
+sudo apt-get -y autoremove xauth
+sudo apt-get -y autoremove debian-reference-common
+sudo apt-get -y autoremove samba-common
+sudo apt-get -y autoremove fontconfig
+sudo apt-get -y autoremove fontconfig-config
+sudo apt-get -y autoremove fonts-freefont-ttf
+sudo apt-get -y autoremove netsurf-gtk
+sudo apt-get -y autoremove netsurf-common
+sudo apt-get -y autoremove dbus-x11
+sudo apt-get -y autoremove desktop-base
+sudo apt-get -y autoremove desktop-file-utils
+sudo apt-get -y autoremove libxmuu1
+sudo apt-get -y install python2.7
+
+sudo rm -rf /opt/vc/src
+sudo rm -rf /usr/share/icons/*
+sudo rm -rf /usr/share/sounds/
+sudo rm -rf /usr/share/squeak/
+sudo rm -rf /usr/share/wallpapers
+sudo rm -rf /usr/share/themes
+sudo rm -rf /usr/share/kde4
+sudo rm -rf /usr/share/images/*
+
+sudo apt-get -y autoremove
+sudo apt-get -y clean
